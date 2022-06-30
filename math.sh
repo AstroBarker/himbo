cbd5bc2611399a68927362d2d23ba1ba190d58ed
Brandon Barker <barker49@msu.edu>
2022-06-29 22:31:58 -0400

    new math kernels

--- math.sh
+++ math.sh
@@ -2,12 +2,27 @@
 
 # math functions. mostly wrappers around bc
 
+# use bc to address equality
+# needed because bash equality struggles with interger - float comparison
+function Eq {
+  if [ $# -gt 2 ]; then
+    echo " ! Too many args to Eq"
+    exit
+  fi
+  echo "$1==$2" | bc
+}
+
 # $1^$2
 function Pow {
-  if [ "$2" -eq 0 ] || [ "$1" == 1.0 ] || [ "$1" == 1 ]; then
+  if [ $# -gt 2 ]; then
+    echo " ! Too many args to Eq"
+    exit
+  fi
+
+  if [ "$(Eq $2 0)" == 1 ] || [ "$(Eq $1 1)" == 1 ]; then
     echo "1.0"
     exit
-  elif [ "$1" == 0.0 ] || [ "$1" == 0 ]; then
+  elif [ "$(Eq $1 0)" == 1 ]; then
     echo "0.0"
     exit
   else
@@ -16,18 +31,38 @@ function Pow {
 }
 
 function Add {
-
+  if [ $# -gt 2 ]; then
+    echo " ! Too many args to Add"
+    exit
+  fi
+  echo "scale=15;$1+$2" | bc
 }
 
 function Subtract {
   # call add
+  if [ $# -gt 2 ]; then
+    echo " ! Too many args to Subtract"
+    exit
+  fi
+  echo "scale=15;$1-$2" | bc
 }
 
 function Mult {
-  
+  if [ $# -gt 2 ]; then
+    echo " ! Too many args to Mult"
+    exit
+  fi
+  echo "scale=15;$1*$2" | bc
 }
 
 function Div {
-
+  if [ $# -gt 2 ]; then
+    echo " ! Too many args to Div"
+    exit
+  fi
+  if [ "$(Eq $2 0)" == 1 ]; then
+    echo " ! You cannot divide by zero!!! Aborting..."
+    exit
+  fi
+  echo "scale=15;$1/$2" | bc
 }
-
